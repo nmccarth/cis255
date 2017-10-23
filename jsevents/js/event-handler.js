bc1d46fd7b4da34a1512dc0baf64d82ff7f66e33
Neal McCarthy <nmccarth@csis.svsu.edu>
2017-10-23 18:31:30 -0400

    Used method from event-attributes to solve

--- jsevents/js/event-handler.js
+++ jsevents/js/event-handler.js
@@ -1,3 +1,4 @@
+/*
 function checkUsername() {                            // Declare function
   var elMsg = document.getElementById('feedback');    // Get feedback element
   if (this.value.length < 5) {                        // If username too short
@@ -6,6 +7,40 @@ function checkUsername() {                            // Declare function
     elMsg.textContent = '';                           // Clear message
   }
 }
+*/
 
-var elUsername = document.getElementById('username'); // Get username input
-elUsername.onblur = checkUsername;  // When it loses focus call checkuserName()
+function checkUsername() {                                                                                                                                                                                                            
+	var name = this.id;
+	var alpha = name.substr(name.length - 2);
+	var output = 'feedback' + alpha;
+ 
+	var start = alpha.substr(0,1).toUpperCase();
+	var end = alpha.substr(1).toUpperCase();
+	var regexp = new RegExp("^["+start+"-"+end+"]+$");
+ 
+	var elMsg = document.getElementById(output);
+ 
+	var elUsername = this.value.toUpperCase();
+ 
+	if (regexp.test(elUsername)) {
+		elMsg.textContent = '';
+	} else {
+		elMsg.textContent = 'The name can only contain '+start+' to '+end;
+	}
+}
+
+var elUsernameac = document.getElementById('usernameac'); 
+elUsernameac.onblur = checkUsername; 
+
+var elUsernamedh = document.getElementById('usernamedh'); 
+elUsernamedh.onblur = checkUsername; 
+
+var elUsernameim = document.getElementById('usernameim');
+elUsernameim.onblur = checkUsername; 
+
+var elUsernamenp = document.getElementById('usernamenp'); 
+elUsernamenp.onblur = checkUsername; 
+
+var elUsernameqz = document.getElementById('usernameqz'); 
+elUsernameqz.onblur = checkUsername; 
+
--- jsevents/js/event-listener.js
+++ jsevents/js/event-listener.js
@@ -1,3 +1,4 @@
+/*
 function checkUsername() {                             // Declare function
   var elMsg = document.getElementById('feedback');     // Get feedback element
   if (this.value.length < 5) {                         // If username too short
@@ -6,7 +7,39 @@ function checkUsername() {                             // Declare function
     elMsg.textContent = '';                            // Clear msg
   }
 }
+*/
 
-var elUsername = document.getElementById('username');  // Get username input
-// When it loses focus call checkUsername()
-elUsername.addEventListener('blur', checkUsername, false);
+function checkUsername() {                                                                                                                                                                                                                     
+        var name = this.id;
+        var alpha = name.substr(name.length - 2); 
+        var output = 'feedback' + alpha;
+ 
+        var start = alpha.substr(0,1).toUpperCase();
+        var end = alpha.substr(1).toUpperCase();
+        var regexp = new RegExp("^["+start+"-"+end+"]+$");
+ 
+        var elMsg = document.getElementById(output);
+ 
+        var elUsername = this.value.toUpperCase();
+ 
+        if (regexp.test(elUsername)) {
+                elMsg.textContent = ''; 
+        } else {
+                elMsg.textContent = 'The name can only contain '+start+' to '+end;
+        }
+}
+
+var elUsernameac = document.getElementById('usernameac');
+elUsernameac.addEventListener('blur', checkUsername, false);
+
+var elUsernamedh = document.getElementById('usernamedh');
+elUsernamedh.addEventListener('blur', checkUsername, false);
+
+var elUsernameim = document.getElementById('usernameim');
+elUsernameim.addEventListener('blur', checkUsername, false);
+
+var elUsernamenp = document.getElementById('usernamenp');
+elUsernamenp.addEventListener('blur', checkUsername, false);
+
+var elUsernameqz = document.getElementById('usernameqz');
+elUsernameqz.addEventListener('blur', checkUsername, false);
