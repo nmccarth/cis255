d039acac14a56432878e16885f0efa34852d662a
Neal McCarthy <nmccarth@csis.svsu.edu>
2017-11-10 15:57:49 -0500

    Added broken swiping code for carousel

--- csis-mobile/js/nmccarth.js
+++ csis-mobile/js/nmccarth.js
@@ -2,3 +2,25 @@
 function goBack() {
 	window.history.back();
 }
+
+
+//carousel functionality
+$('.carousel').carousel({
+	  interval: 2000
+})
+
+
+//mobile swiping in carousel currently unworking 
+/*
+$(".carousel").swipe( {
+	//Generic swipe handler for all directions
+	swipeLeft:function(event, direction, distance, duration, fingerCount) {
+		$(this).parent().carousel('prev'); 
+	},
+	swipeRight: function() {
+		$(this).parent().carousel('next'); 
+	},
+	//Default is 75px, set to 0 for demo so any distance triggers swipe
+	threshold:0
+});
+*/
