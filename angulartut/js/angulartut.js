601894dfe197be4c1ab116806e136f1a0a9782df
Neal McCarthy <nmccarth@csis.svsu.edu>
2017-11-13 13:46:38 -0500

    Angular Example

---
+++ angulartut/js/angulartut.js
@@ -0,0 +1,16 @@
+var app1 = angular.module('app1', []);
+
+app1.controller('ctrl1', function($scope) {
+
+	$scope.first = 1;
+	$scope.second = 1;
+
+	$scope.updateValue = function() {
+		$scope.calculation = $scope.first + ' + ' + $scope.second +
+			" = " + (+$scope.first + +$scope.second);
+	};
+	$scope.updateHypotenuse = function() {
+		$scope.hypotenuse = Math.sqrt(($scope.first * $scope.first) + ($scope.second * $scope.second))
+	};
+
+});
