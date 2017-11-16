082f1ad65f81d1a40c813d5f1b3d39f268a33d6b
Neal McCarthy <nmccarth@csis.svsu.edu>
2017-11-15 21:27:20 -0500

    Parsing multiple prefix w/ space delim but has duplicates

--- angular02/js/angulartut02.js
+++ angular02/js/angulartut02.js
@@ -1,10 +1,10 @@
+
 // ---------- AJAX CALL ----------
 var req = new XMLHttpRequest(); // create an ajax request
 var baseurl = 'https://api.svsu.edu/courses?prefix=';
 var prefix = 'CS';
 var endurl = '&term=17/FA'
 var url = baseurl + prefix + endurl;
-var jsonObj; 
 req.onreadystatechange = function() {
 	var readyStateDone = 4;
 	var readyStatusSuccess = 200;
@@ -25,43 +25,51 @@ function sleep(milliseconds) {
   }
    // alert("woke up!");
 }
-sleep(3000);
+sleep(500);
 
+
+//adding this to the page with javascript seems to get rid of leading problems
 var mydiv = document.getElementById("myclasses");
 mydiv.innerHTML += '<table><tr ng-repeat="course in courses" ng-class="{stripedblue:  course.courseNumber % 2 == 0, stripedbeige: course.courseNumber % 2 != 0}"> <td>{{course.prefix}}</td><td>{{course.courseNumber}}</td></tr></table>'
 
-// console.log(typeof jsonObj);
-// while(jsonObj === undefined) {
-// 	console.log(typeof jsonObj);
-// 	sleep(100);
-// }
 
 // Define the AngularJS Module
 var app4 = angular.module('app4', []);
 
 // Define the Controller and implement the Scope
 app4.controller('eventCtrl', function($scope) {
-  
-// 	var flag = true;
-// 	while(flag) {
-// 		if(typeof jsonObj === undefined){ 
-// 			sleep(500);
-// 			console.log("waiting");
-// 		} else {
-// 			$scope.courses = jsonObj.courses;
-// 			flag = false;
-// 		}
-// 	}
-
-
+	$scope.getJson = function(url) {
+		jQuery.getJSON(url, function( data ) {
+			var old = jsonObj.courses;
+			var new1 = data.courses;
+			var new2 = old.concat(new1);
+			jsonObj = [];
+			jsonObj["courses"] = new2;
+			//jsonObj = [{"courses": old.concat(new1)}];
+		})
+	};
 	
 	$scope.courses = jsonObj.courses;
 	$scope.updatePrefix = function() {
 		prefix = document.getElementById("prefix").value;
-		if (prefix == '') prefix = "CS";
-		url = baseurl + prefix + endurl;
-		req.open('GET', url, true);
-		req.send();
-		$scope.courses = jsonObj.courses;
+		if(/^[a-zA-Z]+$/.test(prefix)) {
+			if (prefix == '') prefix = "CS";
+			url = baseurl + prefix + endurl;
+			req.open('GET', url, true);
+			req.send();
+			$scope.courses = jsonObj.courses;
+		} else if(/^[a-zA-Z\s]+$/.test(prefix)) {
+			var splitprefix = prefix.split(" ");
+			for(var i = 0; i < splitprefix.length; i++){
+				url = baseurl + splitprefix[i] + endurl;
+				if (prefix == 0) {
+					req.open('GET', url, true);
+					req.send();
+				} else {
+					$scope.getJson(url);
+				}
+			}
+			$scope.courses = jsonObj.courses;
+		}
 	}
 });
