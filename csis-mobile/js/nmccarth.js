43c65d0a3bd732ed7c6a2cf2bef3befc601534fa
Neal McCarthy <nmccarth@csis.svsu.edu>
2017-11-20 12:36:20 -0500

    Weather for csis-mobile

--- csis-mobile/js/nmccarth.js
+++ csis-mobile/js/nmccarth.js
@@ -5,22 +5,49 @@ function goBack() {
 
 
 //carousel functionality
-$('.carousel').carousel({
-	  interval: 2000
+jQuery('.carousel').carousel({
+	interval: 2000
 })
 
 
-//mobile swiping in carousel currently unworking 
-/*
-$(".carousel").swipe( {
-	//Generic swipe handler for all directions
-	swipeLeft:function(event, direction, distance, duration, fingerCount) {
-		$(this).parent().carousel('prev'); 
-	},
-	swipeRight: function() {
-		$(this).parent().carousel('next'); 
-	},
-	//Default is 75px, set to 0 for demo so any distance triggers swipe
-	threshold:0
+
+jQuery().ready(function() {
+	if(!jQuery( 'body' ).hasClass( "news" )) {
+		return false; //break out so this weather code won't try to run on other pages
+	}
+	jQuery.simpleWeather({
+		location: '',
+		woeid: '12778949',
+		unit: 'f',
+		success: function(weather) {
+			html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+			html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+			html += '<li class="currently">'+weather.currently+'</li>';
+			html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+
+			$("#weather").html(html);
+			weatherTooltip();
+		},
+		error: function(error) {
+			jQuery("#weather").html('<p>'+error+'</p>');
+		}
+	});
 });
-*/
+
+var weatherTooltip = function() {
+	var degree = jQuery("#weather").html();
+	degree = degree.substr(27,30); //Don't ask why this returns what it does
+	degree = degree.substr(0,2);
+	degree = (degree - 32) * (5 / 9);
+	degree = degree.toFixed(2);
+	jQuery("#weather").attr("title", "Celsius: "+degree+"°C");
+}
+
+jQuery("#weather").click(function () {
+	  var $title = $(this).find(".title");
+	  if (!$title.length) {
+		       alert(jQuery(this).attr("title"));
+		    } else {
+			        $title.remove();
+			      }
+});
